6907374f6ad756f317845840acc319bd1bf0d938
Connor Dunham <cjdunham2112@gmail.com>
2021-11-27 22:14:18 -0800

    removes unused code

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 import getBadges from './BadgeFetch.js';
 import BMerkle from './BMerkle.js';
 
-const args = process.argv;
-const GQL_ENDPOINT = args[3];
-const GQL_QUERY = args[2];
-
 async function fetchBadgesAndGenerateTree(query, gqlEndpoint) {
   const badges = await getBadges(query, gqlEndpoint);
   const mTree = badgesToMerkleTree(badges);
