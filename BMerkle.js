6bdb6667372d92c77856c8124769e0775fa0a6a7
Connor Dunham <cjdunham2112@gmail.com>
2021-11-27 21:29:13 -0800

    replaces sha256 with keccak256

--- BMerkle.js
+++ BMerkle.js
@@ -2,19 +2,25 @@ import etherspkg from 'ethers';
 const { ethers } = etherspkg;
 import merkleTreepkg from 'merkletreejs';
 const { MerkleTree } = merkleTreepkg;
+import keccak256pkg from 'keccak256';
+const { keccak256 } = keccak256pkg;
 
 
 function hashBadge(badgeStruct) {
-  return ethers.utils.soliditySha256(
+  return ethers.utils.solidityKeccak256(
     ['address', 'uint16', 'string'],
-    [badgeStruct.winner.id, badgeStruct.globalBadgeNumber, badgeStruct.definition.id],
+    [badgeStruct.winner.id, badgeStruct.globalBadgeNumber, badgeStruct.definition.id]
   );
 }
 
 function merkleTreeForBadges(badges) {
   const leaves = badges.map(badge => (hashBadge(badge)));
-  const tree = new MerkleTree(leaves, MerkleTree.SHA256);
+  const tree = new MerkleTree(leaves, keccak256, { sort: true });
   return tree;
+}
+
+function badgeToString(badge) {
+  return "badge definition: " + badge.definition.id + "\nbadge winner: " + badge.winner.id + "\tbadge number: " + badge.globalBadgeNumber;
 }
 
 export default { merkleTreeForBadges, hashBadge };
